aa66f78edd916b45cd6b3dd8f7a83a9876320154
itsmearun1 <arundevvb1@gmail.com>
2023-10-09 11:38:43 +0530

    2nd

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express= require("express");
+const express = require("express");
 const { connection } = require("./db");
 const interviewRouter = require("./routes/interviewRouter");
-require("dotenv").config()
-const PORT = process.env.PORT||8080
-const app= express();
-app.use(express.json())
-app.use(require("cors")())
-app.use("/interview",interviewRouter)
-app.get("/",(req,res)=>{
-    res.setHeader("content-type","text/html")
-    res.send("<h1>Welcome to Master base Interview</h1>")
-})
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(express.json());
+app.use(require("cors")());
+app.use("/interview", interviewRouter);
+app.get("/", (req, res) => {
+  res.setHeader("content-type", "text/html");
+  res.send("<h1>Welcome to Master base Interview</h1>");
+});
 
-app.listen(PORT,async()=>{
-    try {
-        await connection 
-        console.log("connected to the Data-Base");
-    } catch (error) {
-        
-    }
-    console.log(`server is running on port ${PORT}`)
-})
+async function startServer() {
+  try {
+    await connection;
+    console.log("Connected to the Database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to the Database:", error);
+  }
+}
+
+startServer();
